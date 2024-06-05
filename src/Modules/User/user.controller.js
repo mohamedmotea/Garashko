2a30599f5f69e6b94fe43854d417a5be920af794
Mohamed Motea <mrte3a@gmail.com>
2024-06-05 15:14:18 +0300

    User

--- src/Modules/User/user.controller.js
+++ src/Modules/User/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../../../DB/Models/user.model.js"
+import ApiFeatures from "../../utils/api-feature.js"
 
 
 export const getAccountData = async (req,res,next) => {
@@ -19,4 +20,19 @@ export const getSpecialAccount = async (req,res,next) => {
   if(!account) return next(new Error('Account not found',{cause:404}))
   // return the account data
   res.status(200).json({message:'fetched account details',data:account,success:true})
+}
+
+export const users = async (req,res,next) => {
+  const {page,size,...search} = req.query
+  const apiFeature = new ApiFeatures(req.query,User.find({},'-password')).pagination({page,size}).search(search)
+  const users = await apiFeature.mongooseQuery
+  res.status(200).json({message:'fetched users details',data:users,success:true})
+}
+export const deleteUser = async (req,res,next) => {
+  const {accountId} = req.params
+  // get the account
+  const account = await User.findById(accountId)
+  if(!account) return next(new Error('Account not found',{cause:404}))
+
+  res.status(200).json({message:'fetched users details',data:true,success:true})
 }
--- src/Modules/User/user.router.js
+++ src/Modules/User/user.router.js
@@ -9,7 +9,9 @@ import { role } from "../../utils/system-rules.js";
 const router = Router()
 
 router
+.get('/users',auth(role.SUPERADMIN),expressAsyncHandler(UC.users))
 .get('/:accountId',vld(validationSchema.getSpecialAccount),expressAsyncHandler(UC.getSpecialAccount))
 .get('/',vld(validationSchema.accountData),auth(Object.values(role)),expressAsyncHandler(UC.getAccountData))
+.delete('/:accountId',auth(role.SUPERADMIN),expressAsyncHandler(UC.deleteUser))
 
 export default router
